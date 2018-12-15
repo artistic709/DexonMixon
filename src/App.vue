eaed04a7dd019b59f3c78457d3e7517a84722a67
Andy Pan <andy0130tw@yahoo.com.tw>
2018-12-15 23:10:23 +0800

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+
+import Web3 from 'web3'
+import utils from './utils'
+
+const web3 = utils.getWeb3Instance()
+
+function hi() {
+
+  utils.getCurrentAddress().then(x => {
+    return web3.eth.getBalance(web3.eth.defaultAccount)
+  }).then(bal => {
+    alert(`account: ${web3.eth.defaultAccount}
+           balance: ${Web3.utils.fromWei(bal)} DEX`)
+  })
+}
+
+window.hi = hi
+</script>
+
+<style>
+</style>
