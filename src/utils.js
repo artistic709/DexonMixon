6239423ba333c4697f72b24df7f1f6e1d5ed579a
Philip Ping-yu Chou <pcchou@pcchou.me>
2018-12-16 04:03:31 +0800

    Use airbnb eslint

--- src/utils.js
+++ src/utils.js
@@ -1,53 +1,50 @@
-import Web3 from 'web3'
+import Web3 from 'web3';
 
-let web3 = null
+let web3 = null;
 
 function getDexonProvider() {
-  const dexonProvider = window.dexon
-  if (!dexonProvider)
-    return null
+  const dexonProvider = window.dexon;
+  if (!dexonProvider) return null;
 
-  if (web3)
-    web3.setProvider(dexonProvider)
+  if (web3) web3.setProvider(dexonProvider);
 
-  return dexonProvider
+  return dexonProvider;
 }
 
 function getWeb3Instance() {
   if (!web3) {
-    web3 = new Web3()
-    getDexonProvider()
+    web3 = new Web3();
+    getDexonProvider();
   }
-  return web3
+  return web3;
 }
 
 function getCurrentAddress() {
-  getWeb3Instance()
-  const prov = getDexonProvider()
+  getWeb3Instance();
+  const prov = getDexonProvider();
 
   return Promise.resolve()
-  .then(() => {
-    if (web3.currentProvider.enable) {
-      return web3.currentProvider.enable()
-    } else {
-      return web3.eth.getAccounts()
-    }
-  })
-  .then(accList => {
-    if (accList.length) {
-      web3.eth.defaultAccount = accList[0]
-    }
-    return Promise.resolve(accList)
-  })
+    .then(() => {
+      if (web3.currentProvider.enable) {
+        return web3.currentProvider.enable();
+      }
+      return web3.eth.getAccounts();
+    })
+    .then((accList) => {
+      if (accList.length) {
+        web3.eth.defaultAccount = accList[0];
+      }
+      return Promise.resolve(accList);
+    });
 }
 
 // sometimes we may expected the callback be called for multiple times,
 // thus does not return a promise by design
 function watchAccountChange(web3, callback) {
   return setInterval(() => {
-    web3.eth.getAccounts().then(acc => {
+    web3.eth.getAccounts().then((acc) => {
       if (!acc || acc[0] != web3.eth.defaultAccount) {
-        callback(acc)
+        callback(acc);
       }
     });
   }, 500);
@@ -58,4 +55,4 @@ export default {
   getWeb3Instance,
   getCurrentAddress,
   watchAccountChange,
-}
+};
