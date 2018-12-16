e938fed3b86fd7b0762ee540e5df1f22f9c2a604
Philip Ping-yu Chou <pcchou@pcchou.me>
2018-12-16 11:38:48 +0800

    Revert vuex changes, we go for global vars

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,19 +1,44 @@
 <template>
-  <div id="form">
+  <div id="formView">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="alert alert-success" role="alert">
+            Address: {{}}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Web3 from 'web3'
+import utils from '../utils'
+
 export default {
   name: 'Form',
   props: {
   },
+  computed: {
+    address() {
+      utils.getCurrentAddress().then(x => {
+              return this.web3.eth.getBalance(this.web3.eth.defaultAccount)
+            })
+    }
+  },
+  methods: {
+  }
 };
 </script>
 
 <style lang="scss">
-#form {
+#formView {
   display: flex;
+  height: 100%;
+}
+
+#formview .container {
   height: 100%;
 }
 </style>
