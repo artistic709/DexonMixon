a3017b819adfd683ed30cea46fa9332ffadab5c8
Philip Ping-yu Chou <pcchou@pcchou.me>
2018-12-16 11:38:48 +0800

    Revert vuex changes, we go for global vars

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@
     <div class="container-fluid h-100">
       <div class="row h-100">
         <div class="col-3 col-panel">
-          <transition name="fade" mode="out-in" appear>
-          <component :is="view"></component>
-          </transition>
+          <div id="home">
+            <div class="jumbotron">
+              <h1 class="display-2">DEXON Mixon!</h1>
+              <p class="lead faded">DEXON Mixon, a decentralized app powered by the DEXON blockchain.</p>
+              <hr class="faded">
+              <p class="faded">This is a new experimental coin mixing service, featuring fantastic on-demand coin fixing (ODCF) capabilities, supporting multiple high-speed (HS) payments.</p>
+            </div>
+            <components :is="view"></components>
+          </div>
         </div>
         <div class="col col-wheel">
           <Wheel></Wheel>
@@ -17,6 +23,7 @@
 
 <script>
 import Web3 from 'web3'
+import * as jQuery from 'jquery'
 import utils from './utils'
 
 import Wheel from './components/Wheel.vue'
@@ -33,26 +40,31 @@ export default {
       view: 'Home'
     }
   },
+  computed: {
+  },
+  mounted() {
+    window.hi = this.hi;
+  },
   methods: {
     changeView() {
       this.view = 'Form';
+      window.web3 = utils.getWeb3Instance();
+      document.querySelectorAll('.faded').forEach((element) => {
+        jQuery('.faded').fadeOut(300);
+        jQuery('.jumbotron').addClass('blank');
+      });
     },
+    hi() {
+      window.web3 = utils.getWeb3Instance();
+      utils.getCurrentAddress().then(x => {
+        return web3.eth.getBalance(web3.eth.defaultAccount)
+      }).then(bal => {
+        alert(`account: ${web3.eth.defaultAccount}
+               balance: ${Web3.utils.fromWei(bal)} DEX`)
+      })
+    }
   }
 }
-
-const web3 = utils.getWeb3Instance()
-
-function hi() {
-
-  utils.getCurrentAddress().then(x => {
-    return web3.eth.getBalance(web3.eth.defaultAccount)
-  }).then(bal => {
-    alert(`account: ${web3.eth.defaultAccount}
-           balance: ${Web3.utils.fromWei(bal)} DEX`)
-  })
-}
-
-window.hi = hi
 </script>
 <style>
 body {
@@ -66,6 +78,10 @@ body {
   height: 100vh;
 }
 
+#home {
+  height: 100%;
+}
+
 .col-panel {
   padding: 0;
   background-color: #fafafa;
@@ -77,9 +93,15 @@ body {
 }
 
 .v-leave { opacity: 1; }
-.v-leave-active { transition: opacity 1s }
+.v-leave-active { transition: opacity 0.3s ease-in-out; }
 .v-leave-to { opacity: 0; }
 .v-enter { opacity: 0; }
-.v-enter-active  { transition: opacity 1s }
+.v-enter-active  { transition: opacity 0.3s ease-in-out; }
 .v-enter-to { opacity: 1; }
+
+.blank {
+  background: none;
+  transition: background 0.3s ease-in-out;
+}
+
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,6 @@
 <template>
-  <div id="home">
-    <div class="jumbotron">
-      <h1 class="display-2">DEXON Mixon!</h1>
-      <p class="lead">DEXON Mixon, a decentralized app powered by the DEXON blockchain.</p>
-      <hr>
-      <p>This is a new experimental coin mixing service, featuring fantastic on-demand coin fixing (ODCF) capabilities, supporting multiple high-speed (HS) payments.</p>
-    </div>
-
-    <div style="padding: 0 20px;">
-      <button type="button" class="btn-lg btn-block btn-dark btn-start">Start</button>
-    </div>
+  <div style="padding: 0 20px;">
+    <button type="button" class="btn-lg btn-block btn-dark btn-start" @click="$parent.changeView()">Start</button>
   </div>
 </template>
 
@@ -31,6 +22,7 @@ export default {
 }
 
 #home .btn-start {
+  cursor: pointer;
 }
 
 #home h1 {
